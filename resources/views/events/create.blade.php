e1db5ab2edeb105b2a1eb8b114247c8cb75524c1
Your Name <camila_coradi@hotmail.com>
2025-05-22 16:52:45 -0300

    Tornar todos os campos obrigatórios no formulário de criação de evento e adicionar opção Nenhum item específico é necessário na infraestrutura

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -7,30 +7,30 @@
             @csrf
              <div class="form-group">
                 <label for="image">Imagem da Atividade:</label>
-                <input type="file" id="image" name="image" class="form-control-file">
+                <input type="file" id="image" name="image" class="form-control-file" required>
             </div>
             <div class="form-group">
                 <label for="title">Evento:</label>
-                <input type="text" class="form-control" id="title" name="title" placeholder="Nome da atividade">
+                <input type="text" class="form-control" id="title" name="title" placeholder="Nome da atividade" required>
             </div>
             <div class="form-group">
                 <label for="date">Data do Evento:</label>
-                <input type="date" class="form-control" id="date" name="date">
+                <input type="date" class="form-control" id="date" name="date" required>
             </div>
             <div class="form-group">
                 <label for="title">Cidade:</label>
-                <input type="text" class="form-control" id="city" name="city" placeholder="Local da atividade">
+                <input type="text" class="form-control" id="city" name="city" placeholder="Local da atividade" required>
             </div>
             <div class="form-group">
                 <label for="title">A atividade é privada?</label>
-                <select name="private" id="private" class="form-control">
+                <select name="private" id="private" class="form-control" required>
                   <option value="0">Não</option>
                   <option value="1">Sim</option>
                 </select>
             </div>
             <div class="form-group">
                 <label for="title">Descrição:</label>
-                <textarea name="description" id="description" class="form-control"></textarea>
+                <textarea name="description" id="description" class="form-control" required></textarea>
             </div>
               <div class="form-group">
                 <label for="title">Adicione itens de infraestrutura:</label>
@@ -48,6 +48,9 @@
                 </div>
                 <div class="form-group">
                     <input type="checkbox" name="items[]" value="Colchonetes"> Colchonetes
+                </div>
+                <div class="form-group">
+                    <input type="checkbox" name="items[]" value="Nenhum item específico é necessário para esta atividade"> Nenhum item específico é necessário para esta atividade
                 </div>
             </div>
             <br>
